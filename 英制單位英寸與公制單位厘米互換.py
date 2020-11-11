427826b751792bc530f6a6b936d55cd3ad4fcfe0
方柏傑 <62335065+fangboja@users.noreply.github.com>
2020-11-11 19:01:57 +0800

    Delete 英制單位英寸與公制單位厘米互換.py

--- 英制單位英寸與公制單位厘米互換.py
+++
@@ -1,9 +0,0 @@
-value=float(input("請輸入長度"))
-unit=input("輸入單位")
-
-if unit=="in" or unit =="英寸":
-    print("%f英寸=%f厘米"%(value,value*2.54))
-elif unit == "cm" or unit=="厘米":
-    print("%f厘米=%f英吋" % (value, value/2.54))
-else:
-    print("輸入錯誤")
